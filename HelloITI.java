158fe235eca795501bda540820ff74e3c37a3af0
Ahmed_El_Sayyad <elsayyada197@gmail.com>
2025-12-30 12:09:23 +0200

    add goodbye method in hello iti class

--- HelloITI.java
+++ HelloITI.java
@@ -8,7 +8,10 @@ class HelloITI{
 	public void printHelloGit(){
       System.out.println("HelloGit");
 	}
-	
+
+	 public void printCoodBye(){
+		 System.out.println("Bye");
+	 }	
 	public static void main(String args[]){
 		printHelloITI();
 		printHelloGit();
